e928b32999176d6b340d3bff6c4491ca18c9c881
Paras <3waysourcetech@gmail.com>
2023-04-30 16:10:06 +0530

    test

--- src/sockets/dogs.socket.js
+++ src/sockets/dogs.socket.js
@@ -3,19 +3,6 @@ const io_client = require('socket.io-client');
 // Vars
 let socket = null;
 let io = null;
-
-// Constructor
-export default (_socket, _io) => {
-  socket = _socket;
-  io = _io;
-  on();
-};
-
-function joinRoom(data) {
-  var mcx_scripts = [data];
-  var done_scripts = [];
-
-}
 const socket_client = io_client('ws://5.22.221.190:8000', {
   transports: ['websocket'],
   extraHeaders: {
@@ -32,6 +19,18 @@ const socket_client2 = io_client('ws://5.22.221.190:5000', {
     Referer: 'http://localhost:8000'
   }
 });
+// Constructor
+export default (_socket, _io) => {
+  socket = _socket;
+  io = _io;
+  on();
+};
+
+function joinRoom(data) {
+  var mcx_scripts = [data];
+  var done_scripts = [];
+}
+
 socket_client2.on('error', (error) => {
   // Handle the error here
   console.error(error);
@@ -39,12 +38,12 @@ socket_client2.on('error', (error) => {
 
 socket_client.on('stock', async (data) => {
   console.log(data, 'bt met');
-  io.emit('stock', data);
+  io.emit(data.name, data);
 });
 
 socket_client2.on('stock', async (data) => {
   console.log(data, 'bt met');
-  io.emit('stock', data);
+  io.emit(data.name, data);
 });
 
 var joinedScript = [];
