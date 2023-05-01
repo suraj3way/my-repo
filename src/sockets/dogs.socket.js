35afd77c48267dc57107375810c0d03f024fa82d
Paras <3waysourcetech@gmail.com>
2023-05-01 10:49:46 +0530

    test

--- src/sockets/dogs.socket.js
+++ src/sockets/dogs.socket.js
@@ -3,22 +3,7 @@ const io_client = require('socket.io-client');
 // Vars
 let socket = null;
 let io = null;
-const socket_client = io_client('ws://5.22.221.190:8000', {
-  transports: ['websocket'],
-  extraHeaders: {
-    Referer: 'http://localhost:8000'
-  }
-});
-socket_client.on('error', (error) => {
-  // Handle the error here
-  console.error(error);
-});
-const socket_client2 = io_client('ws://5.22.221.190:5000', {
-  transports: ['websocket'],
-  extraHeaders: {
-    Referer: 'http://localhost:8000'
-  }
-});
+
 // Constructor
 export default (_socket, _io) => {
   socket = _socket;
@@ -26,31 +11,46 @@ export default (_socket, _io) => {
   on();
 };
 
-function joinRoom(data) {
-  var mcx_scripts = [data];
-  var done_scripts = [];
-}
 
-socket_client2.on('error', (error) => {
-  // Handle the error here
-  console.error(error);
-});
 
-socket_client.on('stock', async (data) => {
-  console.log(data, 'bt met');
-  io.emit(data.name, data);
-});
 
-socket_client2.on('stock', async (data) => {
-  console.log(data, 'bt met');
-  io.emit(data.name, data);
-});
 
 var joinedScript = [];
 
 // Listen events
 const on = () => {
- 
+  const socket_client = io_client('ws://5.22.221.190:8000', {
+    transports: ['websocket'],
+    extraHeaders: {
+      Referer: 'http://localhost:8000'
+    }
+  });
+  socket_client.on('error', (error) => {
+    // Handle the error here
+    console.error(error);
+  });
+
+  const socket_client2 = io_client('ws://5.22.221.190:5000', {
+    transports: ['websocket'],
+    extraHeaders: {
+      Referer: 'http://localhost:8000'
+    }
+  });
+  socket_client2.on('error', (error) => {
+    // Handle the error here
+    console.error(error);
+  });
+
+  socket_client.on('stock', async (sc_data) => {
+    console.log(sc_data, 'bt met');
+    io.emit(sc_data.name, sc_data);
+  });
+  
+  socket_client2.on('stock', async (sc_data2) => {
+    console.log(sc_data2, 'bt met');
+    io.emit(sc_data2.name, sc_data2);
+  });
+
   socket.on('join', (data) => {
     // io.emit('dogs:pong', data);
     // joinRoom(data)
