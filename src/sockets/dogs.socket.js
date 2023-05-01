bccab3e6f4b2aa18d9100c79e4fea74f52076194
Paras <3waysourcetech@gmail.com>
2023-05-01 14:35:29 +0530

    latest

--- src/sockets/dogs.socket.js
+++ src/sockets/dogs.socket.js
@@ -1,9 +1,35 @@
-const io_client = require('socket.io-client');
-const socket_client = require('./socket_client');
+// const io_client = require('socket.io-client');
+import ioClient from 'socket.io-client';
 // Vars
 let socket = null;
 let io = null;
+let socketClient = null;
+const connectSocketClient = (url) => {
+  if (!socketClient) {
+    socketClient = ioClient(url, {
+      transports: ['websocket'],
+      extraHeaders: {
+        Referer: 'http://localhost:8000'
+      }
+    });
 
+    socketClient.on('error', (error) => {
+      console.error(error);
+    });
+
+    socketClient.on('stock', (data) => {
+      console.log(data, 'bt met');
+      io.emit('stock', data);
+    });
+  }
+};
+
+const onJoin = (data) => {
+  connectSocketClient('ws://5.22.221.190:8000');
+  connectSocketClient('ws://5.22.221.190:5000');
+
+  socketClient.emit('join', data);
+};
 // Constructor
 export default (_socket, _io) => {
   socket = _socket;
@@ -13,58 +39,15 @@ export default (_socket, _io) => {
 
 
 
-
-
-var joinedScript = [];
-
 // Listen events
 const on = () => {
-  // const socket_client = io_client('ws://5.22.221.190:8000', {
-  //   transports: ['websocket'],
-  //   extraHeaders: {
-  //     Referer: 'http://localhost:8000'
-  //   }
-  // });
-  // socket_client.on('error', (error) => {
-  //   // Handle the error here
-  //   console.error(error);
-  // });
+  console.log('a user connected');
 
-  // const socket_client2 = io_client('ws://5.22.221.190:5000', {
-  //   transports: ['websocket'],
-  //   extraHeaders: {
-  //     Referer: 'http://localhost:8000'
-  //   }
-  // });
-  // socket_client2.on('error', (error) => {
-  //   // Handle the error here
-  //   console.error(error);
-  // });
+  socket.on('join', onJoin);
 
-  socket_client.on('stock', async (sc_data) => {
-    console.log(sc_data, 'bt met');
-    io.emit(sc_data.name, sc_data);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
   });
-  
-  // socket_client2.on('stock', async (sc_data2) => {
-  //   console.log(sc_data2, 'bt met');
-  //   io.emit(sc_data2.name, sc_data2);
-  // });
-
-  socket.on('join', (data) => {
-    // io.emit('dogs:pong', data);
-    // joinRoom(data)
-    if(!joinedScript.includes(data)){
-      joinedScript.push(data);
-      socket_client.emit('join', data);
-
-      // socket_client2.emit('join', data);
-    }
-   
-
-  });
-  
-
 };
 
 export { socket, io };
