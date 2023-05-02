1f326785772d650cdb989d10591ea2b00c65ddaf
Paras <3waysourcetech@gmail.com>
2023-05-02 12:43:35 +0530

    socket updated

--- src/sockets/dogs.socket.js
+++ src/sockets/dogs.socket.js
@@ -4,6 +4,7 @@ import ioClient from 'socket.io-client';
 let socket = null;
 let io = null;
 let socketClient = null;
+let socketClient2 = null;
 const connectSocketClient = (url) => {
   if (!socketClient) {
     socketClient = ioClient(url, {
@@ -24,11 +25,32 @@ const connectSocketClient = (url) => {
   }
 };
 
+const connectSocketClient2 = (url) => {
+  if (!socketClient2) {
+    socketClient2 = ioClient(url, {
+      transports: ['websocket'],
+      extraHeaders: {
+        Referer: 'http://localhost:8000'
+      }
+    });
+
+    socketClient2.on('error', (error) => {
+      console.error(error);
+    });
+
+    socketClient2.on('stock', (data) => {
+      // console.log(data, '50000000000000000000000000000000000000000000000000000000000000000000000000000');
+      io.emit(data.name, data);
+    });
+  }
+};
+
 const onJoin = (data) => {
   connectSocketClient('ws://5.22.221.190:8000');
-  connectSocketClient('ws://5.22.221.190:5000');
+  connectSocketClient2('ws://5.22.221.190:5000');
 
   socketClient.emit('join', data);
+  socketClient2.emit('join', data);
 };
 // Constructor
 export default (_socket, _io) => {
